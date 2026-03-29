5d2d7e3aa0845eeee1555d84cb9cce265e802338
Kevin <kevinxp919@gmail.com>
2026-03-29 19:29:21 +1100

    Update the test runner on github action

--- playwright.api.config.ts
+++ playwright.api.config.ts
@@ -10,11 +10,10 @@ export default defineConfig({
   expect: {
     timeout: 5000
   },
-  failureReason: true,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 8 : undefined,
   reporter: [
     ['html', { outputFolder: 'playwright-report-api' }],
     ['line'],
